5f6fb92bbf83a330023b31e3b8304262bb8e59d0
Rafael Pavanello <rpavanello97@hotmail.com>
2024-08-27 22:13:30 +0100

    Starting Module 3
    Starting Module 3, all videos watched.

---
+++ Java_Programming_Arrays _Lists_And_Structured_Data/Module3/src/Main.java
@@ -0,0 +1,17 @@
+import classes.CountWords;
+import classes.WordFrequencies;
+import classes.WordsWithArrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        WordFrequencies wordFreq = new WordFrequencies();
+//        wordFreq.tester();
+
+//        WordsWithArrays wordsWithArrays = new WordsWithArrays();
+//        wordsWithArrays.tester();
+
+        CountWords cw = new CountWords();
+        cw.tester();
+        cw.randomTester();
+    }
+}
